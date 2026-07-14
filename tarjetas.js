135f26875b0b5cd7b0f88745b59a6fad9f515166
dalimberAndrade <dalimberpubg@gmail.com>
2026-07-13 19:20:19 -0500

    Recuperar valores de inputs para crear tarjetas dentro del rango

--- tarjetas.js
+++ tarjetas.js
@@ -1,10 +1,16 @@
-function crearTrajetas() 
+function crearTarjetas() 
 {
     /*let divTarjetas=document.getElementById("divTarjetas");
     divTarjetas.innerHTML="<h1>MODIFICANDO DIV</h1>";*/
     let contenido="";
     let divTarjetas=document.getElementById("divTarjetas");
-    for (let index = 1; index <=5; index++) 
+    //RECUPERAMOS VALORES DE ELEMENTOS INPUT
+    let cmpTxtDesde=document.getElementById("txtDesde");
+    let cmpTxtHasta=document.getElementById("txtHasta");
+    //CONVERTIMOS VALORES STRING A NUMERO
+    numDesde=parseInt(cmpTxtDesde.value);
+    numHasta=parseInt(cmpTxtHasta.value);
+    for (let index = numDesde; index <=numHasta; index++) 
         {
             contenido=contenido+"<div class='item'>"+index+"</div>";
             divTarjetas.innerHTML=contenido;
