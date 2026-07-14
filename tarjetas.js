294b6e274a2471ee6d354f5d27d0750a9835f87e
dalimberAndrade <dalimberpubg@gmail.com>
2026-07-13 19:26:30 -0500

    Agregar y recuperar input salto y agregar funcionalidad al ciclo for

--- tarjetas.js
+++ tarjetas.js
@@ -7,10 +7,12 @@ function crearTarjetas()
     //RECUPERAMOS VALORES DE ELEMENTOS INPUT
     let cmpTxtDesde=document.getElementById("txtDesde");
     let cmpTxtHasta=document.getElementById("txtHasta");
+    let cmpTxtSalto=document.getElementById("txtSalto");
     //CONVERTIMOS VALORES STRING A NUMERO
     numDesde=parseInt(cmpTxtDesde.value);
     numHasta=parseInt(cmpTxtHasta.value);
-    for (let index = numDesde; index <=numHasta; index++) 
+    numSalto=parseInt(cmpTxtSalto.value);
+    for (let index = numDesde; index <=numHasta; index+=numSalto) 
         {
             contenido=contenido+"<div class='item'>"+index+"</div>";
             divTarjetas.innerHTML=contenido;
